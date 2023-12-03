366142e15bf2b840572f28305027786b813e2a81
Nathan <nathan.perrine@gmail.com>
2023-12-03 03:14:48 -0500

    new ground started, parallax background scroll added

---
+++ scenes/Background.gd
@@ -0,0 +1,7 @@
+extends ParallaxLayer
+
+@export var BACKGROUND_SPEED : float = -15
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.motion_offset.x += BACKGROUND_SPEED * delta
